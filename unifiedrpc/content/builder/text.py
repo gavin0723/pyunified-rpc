fd784af1c94cabbd3bbf104ed512d0b2975471a8
lipixun <lipixun@outlook.com>
2016-06-02 14:26:46 +0800

    Support file for text

--- unifiedrpc/content/builder/text.py
+++ unifiedrpc/content/builder/text.py
@@ -28,5 +28,15 @@ class TextContentBuilder(ContentBuilder):
                 yield value
             elif isinstance(value, unicode):
                 yield value.encode(response.encoding)
+            elif isinstance(value, file):
+                # Read the file
+                while True:
+                    try:
+                        buffer = value.read(4096)
+                        if not buffer:
+                            break
+                        yield buffer
+                    except EOFError:
+                        break
             else:
                 raise ValueError('Unsupported value type [%s] for text content builder' % type(value).__name__)
